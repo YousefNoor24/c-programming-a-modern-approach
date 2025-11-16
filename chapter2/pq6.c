bb6c95a92f24be167b9450a04266b69e6c2a89d9
YousefNoor24 <yousefnoor477@gmail.com>
2025-11-16 18:30:01 +0800

    Edit pq6.c

--- chapter2/pq6.c
+++ chapter2/pq6.c
@@ -5,12 +5,7 @@ int main(void) {
   printf("Enter a value for x: ");
   scanf("%d", &x);
 
-  int powerOfFive = x * x * x * x * x;
-  int powerOfFour = x * x * x * x;
-  int cubicNum = x * x * x;
-  int squaredNum = x * x;
-
-  int total = (3 * powerOfFive) + (2 * powerOfFour) - (5 * cubicNum) - squaredNum + (7 * x) - 6;
+  int total = ((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6;
 
   printf("Value: %d\n", total);
   return 0;
