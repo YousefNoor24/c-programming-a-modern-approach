4616e42abedfac06c8c37239a80100af9c9ef31c
YousefNoor24 <yousefnoor477@gmail.com>
2025-11-17 19:05:28 +0800

    Edit chapter3/pq4.c

--- chapter3/pq4.c
+++ chapter3/pq4.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
- 
+  int teleNum1, teleNum2, teleNum3;
+  
+  printf("Enter phone number [(xxx) xxx-xxxx]: ");
+  scanf("(%d) %d-%d", &teleNum1, &teleNum2, &teleNum3);
+
+  printf("You entered %03d.%03d.%04d\n", teleNum1, teleNum2, teleNum3);
+
   return 0;
 }
