c9089a90f4673022b4f0307f32d469208e34b858
YousefNoor24 <yousefnoor477@gmail.com>
2025-11-17 19:13:11 +0800

    Add chapter3/pq5.c

---
+++ chapter3/pq5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+  int num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12, num13, num14, num15, num16;
+
+  printf("Enter the numbers from 1 to 16 in any order: ");
+  scanf("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d", &num1, &num2, &num3, &num4, &num5, &num6, &num7, &num8, &num9, &num10, &num11, &num12, &num13, &num14, &num15, &num16);
+
+  printf("%d\t%d\t%d\t%d\n%d\t%d\t%d\t%d\n%d\t%d\t%d\t%d\n%d\t%d\t%d\t%d\n", num1, num2, num3, num4, num5, num6, num7, num8, num9, num10, num11, num12, num13, num14, num15, num16);
+
+  return 0;
+}
