9b470c73283831c3af5197d540e92779397e00a3
YousefNoor24 <yousefnoor477@gmail.com>
2025-11-17 18:38:08 +0800

    Edit chapter3/pq2.c

--- chapter3/pq2.c
+++ chapter3/pq2.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(void) {
-  
+  int itemNum, month, day, year;
+  float unitPrice;
+
+  printf("Enter item number: ");
+  scanf("%d", &itemNum);
+  printf("Enter unit price: ");
+  scanf("%f", &unitPrice);
+  printf("Enter purchase date (mm/dd/yyyy): ");
+  scanf("%d/%d/%d", &month, &day, &year);
+
+  printf("Item\t\tUnit\t\tPurchase\n\t\tPrice\t\tDate\n%d\t\t$%8.2f\t%02d/%02d/%04d\n", itemNum, unitPrice, month, day, year);
   return 0;
 }
