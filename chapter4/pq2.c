ecb174d8dddd649eb003e2520f55629c6f2df978
YousefNoor24 <yousefnoor477@gmail.com>
2025-11-18 18:32:18 +0800

    Edit chapter4/pq2.c

--- chapter4/pq2.c
+++ chapter4/pq2.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main(void) {
+  int num, num2;
+  printf("Enter a three-digit number: ");
+  scanf("%d", &num);
   
+  num2 = num % 100;
+
+  printf("The reversal is: %d%d%d\n", num2 % 10, num2 / 10, num / 100);
   return 0;
 }
